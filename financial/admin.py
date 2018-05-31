026622bbd0580a9be412581689c4eaf9b9ce08da
unknown <lh7167@dms.bocoa.com>
2018-05-31 14:35:47 +0800

    Add 3 models

--- financial/admin.py
+++ financial/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import HaoyiAccount, HaoyiTransaction, ObjectAccount
 
 # Register your models here.
+admin.site.register(HaoyiAccount)
+admin.site.register(HaoyiTransaction)
+admin.site.register(ObjectAccount)
