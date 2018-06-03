4b11fe8eb253729db3c44ff3a0eb6d9700a17be1
Hao Luo <leoluo22@gmail.com>
2018-06-03 10:27:46 +0800

    Add specific details

--- financial/models.py
+++ financial/models.py
@@ -17,6 +17,7 @@ class HaoyiTransaction(models.Model):
     # 交易表
     account_id = models.CharField(db_column='ACCOUNT_ID', max_length=50)
     transaction_date = models.DateTimeField(db_column='TRANSACTION_DATE')
+    transaction_timestamp = models.CharField(db_column='TRANSACTION_TIMESTAMP', max_length=20, help_text='交易时间戳', null=True)
     '''
     income = models.FloatField(db_column='INCOME', default=0.0)
     expenditure = models.FloatField(db_column='EXPENDITURE', default=0.0)
@@ -24,11 +25,16 @@ class HaoyiTransaction(models.Model):
     amount = models.FloatField(db_column='AMOUNT', blank=False, help_text='交易金额') 
     transaction_type = models.IntegerField(db_column='TRANSACTION_TYPE', blank=False, help_text='0: 支出 1: 收入 2: 借出 3:还款')
     balance = models.FloatField(db_column='BALANCE', help_text='余额')
+    procedure_fee = models.FloatField(default=0.0, db_column='PROCEDURE_FEE', help_text='手续费')
+    transaction_way = models.CharField(db_column='TRANSACTION_WAY', max_length=10, help_text='交易方式', null=True)
     transaction_bank = models.CharField(db_column='TRANSACTION_BANK', max_length=50, help_text='交易行名')
+    transaction_categoty = models.CharField(db_column='TRANSACTION_CATEGOTY', max_length=10, help_text='交易类别', null=True)
     object_city = models.CharField(db_column='OBJECT_CITY', max_length=50, help_text='对方省市')
     object_account_id = models.CharField(db_column='OBJECT_ACCOUNT_ID', max_length=50, help_text='对方账号')
     object_account_name = models.CharField(db_column='OBJECT_ACCOUNT_NAME', max_length=50, help_text='对方户名')
-    transaction_usage = models.CharField(db_column='TRANSACTION_UASGE', max_length=50, help_text='交易用途')
+    transaction_explain = models.CharField(db_column='TRANSACTION_EXPLAIN', max_length=10, help_text='交易说明', null=True)
+    transaction_summary = models.CharField(db_column='TRANSACTION_SUMMARY', max_length=10, help_text='交易摘要', null=True)
+    transaction_usage = models.CharField(db_column='TRANSACTION_UASGE', max_length=50, help_text='交易用途或交易附言')
     remark = models.CharField(db_column='REMARK', max_length=50, help_text='备注')
     crt_date = models.DateTimeField(db_column='CRT_DATE', auto_now=True)
 
